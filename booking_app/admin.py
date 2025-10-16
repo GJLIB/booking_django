30640fb0b0fad4dd00e63a18d464cdd5790817db
GJLIB <glebnazarov.2008@gmail.com>
2025-10-16 20:54:51 +0300

    +urls

--- booking_app/admin.py
+++ booking_app/admin.py
@@ -6,5 +6,6 @@ admin.site.register(Room)
 class BookingAdmin(admin.ModelAdmin):
     list_display = ('room', 'user', 'phone_number', 'persons', 'check_in', 'check_out')
     search_fields = ('user', 'phone_number', )
-
+    
+    
 admin.site.register(Booking, BookingAdmin)
