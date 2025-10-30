86ddbcf52f07b32fe37effbf4d78d67767deb7cc
GJLIB <glebnazarov.2008@gmail.com>
2025-10-30 20:40:38 +0200

    +register page

---
+++ auth_system/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    phone = models.CharField(max_length = 20)
+    date_of_birth = models.DateField(null = True, blank = True)
+    email = models.EmailField(unique = True)
+    
+    def __str__(self):
+        return self.email
